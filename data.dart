64a4f10c543ab9386ef06698a820338006317ae9
Sagnik Mitra <sagnikmitra123@gmail.com>
2023-06-26 22:15:14 +0530

    Create data.dart

---
+++ data.dart
@@ -0,0 +1,129 @@
+List<Map<String, dynamic>> judge_data = [
+  {
+    "name": "Jyoti Mishra",
+    "designation": "Software Design Engineer-4, Evertz Microsystems",
+    "image":   "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F87d825764afc41d8ab3aff5a70bea67d%2F71.png&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/jyotim29/",
+  },
+  {
+    "name": "Soumyadip Chowdhury",
+    "designation": "Software Engineer, Red Hat",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F9fe1b51c6f274da180219239ce206df4%2F798.png&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/soumyadip-chowdhury/",
+  },
+  {
+    "name": "Anubhav Singh",
+    "designation": "Co- founder, Dynopii",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F47122272bcdf4383bb7672a6ac02e277%2F741.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/xprilion/",
+  },
+  {
+    "name": "Md Nasirul, Haque",
+    "designation": "Systems Engineer, Tata Consultancy Services",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F3c3546e567cd45b5b99c134fc574b8aa%2F62.png&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/mdnasirulhaque/",
+  },
+  {
+    "name": "Sabyasachi Mukhopadhyay",
+    "designation": "PhD Research Scholar, IIT Kharagpur",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F6e5bef7259a84cb8986d366c342b9e09%2F169.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/sabyasachi -mukhopadhyay-303a1027",
+  },
+  {
+    "name": "Kazi Haque",
+    "designation": "Co-Founder, Director, Dynopii",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F0fe463688d024632a97f020ce9f118d5%2F223.png&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/haquekazi/",
+  },
+  {
+    "name": "Gourav Mohanty",
+    "designation": "Full Stack Developer, Shuru.io",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2Fdddc114fdade46b297cd819295fb50aa%2F635.png&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/gourav-mohanty-78006a201",
+  },
+  {
+    "name": "Sabyasachi Ganguly",
+    "designation": "Founder",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2Fb1d2e8e3ad3b4939bf2e78f7a894aabd%2F895.png&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/sabyganguly",
+  },
+  {
+    "name": "Richik Chanda",
+    "designation": "Full Stack Blockchain Developer, Questbook",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F3ecae30384e44fd7a6165d56d71ec236%2F252.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/richikchanda-0585ba15b/",
+  },
+  {
+    "name": "Jaydip Dey",
+    "designation": "SDE - 1, Optum",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F20b4a8c158e446caa24de7cca296ff21%2F144.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/jaydip-dey/",
+  },
+  {
+    "name": "Niloy Sikdar",
+    "designation": "Google Summer of Code'23, Sugar Labs",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F63c9b4acfdfe4997843d156611e1b8b0%2F117.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/niloysikdar/",
+  },
+  {
+    "name": "Akshita Dixit",
+    "designation": "Backend Developer, 1mg",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F9e3eacd1332a472fa2d455fcd9b14e92%2F70.png&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/akshitadixit/",
+  },
+  {
+    "name": "Jyotirmoy Roy",
+    "designation": "GSOC Mentor, Keploy",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F7994adbfc84540c881bae76a86c61dfe%2F789.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/jyotirmoyroy",
+  },
+  {
+    "name": "Advaita Saha",
+    "designation": "Software Engineer, Cipherem",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2Fbc78df1f045544ddb01874f0245c0f04%2F232.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/advaita-saha/",
+  },
+  {
+    "name": "Aryan Agarwal",
+    "designation": "Software Engineer, Lenspost",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2Fdc9fb3ca5c3d4d88b23e909e65db0e06%2F339.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/thearyanag/",
+  },
+  {
+    "name": "Eshan Chattaraj",
+    "designation": "Research Scientist (Engg Div) at R&D , Govt. of India, ex-Google",
+    "image":
+    "https://hack4bengal-2.devfolio.co/_next/image?url=https%3A%2F%2Fassets.devfolio.co%2Fhackathons%2F6e1fdb1f76ff4aa28f3fe89d79d9f618%2Fjudges%2F410ccfb71c77452883dec33311b2137f%2F889.jpeg&w=1440&q=75",
+    "position": "Mentor / Judge",
+    "linkedin": "https://www.linkedin.com/in/eshan-chattaraj-089172108/",
+  }
+];
